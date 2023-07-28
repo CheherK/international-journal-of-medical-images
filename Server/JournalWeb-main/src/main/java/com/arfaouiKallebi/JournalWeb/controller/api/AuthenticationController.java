4d841fa0ec1dd8b5425da0f07033e9c9102a8dba
cheherk <cheherkallebi@gmail.com>
2023-07-28 15:20:11 +0100

    upload server side part

---
+++ Server/JournalWeb-main/src/main/java/com/arfaouiKallebi/JournalWeb/controller/api/AuthenticationController.java
@@ -0,0 +1,23 @@
+package com.arfaouiKallebi.JournalWeb.controller.api;
+
+import com.arfaouiKallebi.JournalWeb.dto.LoginDto;
+import com.arfaouiKallebi.JournalWeb.dto.RegisterDto;
+import com.arfaouiKallebi.JournalWeb.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private IUserService userService ;
+    @PostMapping("register")
+    public ResponseEntity<?> register(@RequestBody RegisterDto registerDto) {
+        return userService.register(registerDto) ;
+    }
+    @PostMapping("login")
+    public String register(@RequestBody LoginDto loginDto) {
+        return userService.authenticate(loginDto) ;
+    }
+}
